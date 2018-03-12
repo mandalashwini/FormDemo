6c605d6a17753e20de5c694ea740deb58327f8ab
Ashwini Mandal <ashwini.mandal@gmail.com>
2018-03-12 19:45:33 +0530

    with new

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   resources :student
    root 'student#index'
     
-    get 'student/show'
+   
   
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
 end
